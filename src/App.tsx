6b5a8ca2a5ca89779949b5d54bd788da7317a509
Kyle Chen <kylecn94@gmail.com>
2026-06-02 03:37:04 +0100

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,9 @@ import { AppUIProvider } from './context/AppUIContext';
 import Sidebar from './components/Sidebar/Sidebar';
 import Sidedraw from './components/Sidedraw/Sidedraw';
 import Page1 from './Page1/Page1'; 
-import { AuthGate } from './auth/AuthGate';
-import { useAuth } from './auth/AuthContext';
 import './App.css';
 
 const App = () => {
-  const { canAccessApp } = useAuth();
-
-  if (!canAccessApp) {
-    return <AuthGate />;
-  }
-
   return (
     <Router basename={import.meta.env.BASE_URL}>
       <AppUIProvider>
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { PublicClientApplication } from '@azure/msal-browser'
-import { MsalProvider } from '@azure/msal-react'
 import './index.css'
 import App from './App.tsx'
-import { msalConfig } from './auth/msalConfig'
-import { AuthProvider } from './auth/AuthContext'
 
-const msalInstance = new PublicClientApplication(msalConfig)
-
-void msalInstance.initialize().then(() => {
-  createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-      <MsalProvider instance={msalInstance}>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </MsalProvider>
-    </StrictMode>,
-  )
-})
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
