6c88d803565c2394730a262685c2d6179c5f67d1
tichen <tichen@fosterandpartners.com>
2026-07-23 15:50:11 +0100

    BBOX Cache

--- src/MapPage/components/Map/DataLayer/inputHooks/onUserRoam.ts
+++ src/MapPage/components/Map/DataLayer/inputHooks/onUserRoam.ts
@@ -34,7 +34,7 @@ const onUserRoam = (
 
       const res = zoomToResolution(zoom);
       // console.log('res', res, 'zoom', zoom);
-      console.log('bbox', sw_lat, sw_lng, ne_lat, ne_lng, 'zoomBucket', zoomBucket);
+      // console.log('bbox', sw_lat, sw_lng, ne_lat, ne_lng, 'zoomBucket', zoomBucket);
       
       setViewportParams({
         // sw_lat: b.getSouth(),
