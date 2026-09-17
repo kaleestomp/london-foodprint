51b2dc115f2e92ebc4f32adc78aa05e306076a35
tichen <tichen@fosterandpartners.com>
2026-09-17 23:17:56 +0100

    rank-fix

--- src/MapPage/components/PlaceDetail/formatDetails/formatRank.ts
+++ src/MapPage/components/PlaceDetail/formatDetails/formatRank.ts
@@ -10,7 +10,7 @@ const formatRank = (
     const rankPercent = `${(rank * 100).toFixed(1)}%`;
     if (rank > 0.45 && rank < 0.55)
         return `Ranked about average in the city (${rankPercent}) based on ${count} reviews`;
-    else if (rank >= 0.55)
+    else if (rank <= 0.45)
         return `Ranked below average in the city (${rankPercent}) based on ${count} reviews`;
     
     return `Ranked Top #${rankPercent} in the city based on ${count} reviews`;
